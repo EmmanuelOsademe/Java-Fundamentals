d04d0f51c20b353152cc6183d6e76d3945bed8f0
Emmanuel Osademe <emma.osademe@gmail.com>
2023-05-06 11:39:07 +0100

    assignment 10

---
+++ src/main/java/week10_assignment/wordcounter/Main.java
@@ -0,0 +1,31 @@
+package week10_assignment.wordcounter;
+
+import java.io.*;
+
+public class Main {
+
+    public static String wordsCounter(String fileName, String searchWord){
+        int counter = 0;
+        String line;
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
+
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+                String[] strs = line.split(" ");
+                for(String word: strs){
+                    if(word.equalsIgnoreCase(searchWord)){
+                        counter++;
+                    }
+                }
+            }
+        }catch (IOException  | NullPointerException e){
+            System.out.println("I/O Exception occurred: " + e);
+            e.printStackTrace();
+        }
+        return (counter + " occurrences of " + searchWord + " were found");
+    }
+
+    public static void main(String[] args){
+       System.out.println(wordsCounter("books.txt", "the"));
+    }
+}
